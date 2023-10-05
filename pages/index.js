e3366ec198799053275a4707b4e396beff73dafb
asadbe0304 <asadbekaxmadkulov@gmail.com>
2023-10-05 20:01:43 +0500

    ok

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ const Home = () => {
       <div className="w-full h-full bg-gradient-to-r from-primary/10 via-black/30 to-black/10">
         <div className="text-center flex flex-col justify-center xl:pt-40 xl:text-left h-full container mx-auto ">
           <motion.h1
-            className="h2"
+            className="text-5xl my-3"
             variants={fadeIn("down", 0.2)}
             initial="hidden"
             animate="show"
@@ -27,11 +27,11 @@ const Home = () => {
             initial="hidden"
             animate="show"
             exit={"hidden"}
-            className="max-w-sm xl:max-w-xl mx-auto xl:mx-0 mb-5 xl:mb-16"
+            className="max-w-sm text-sm xl:max-w-xl mx-auto xl:mx-0 mb-5 xl:mb-16"
           >
             There are many variations of passages of Lorem Ipsum available, but
             the majority have suffered alteration in some form, by injected
-            humour, or randomised words which look even slightly believable.
+            humour.
           </motion.p>
           <div className="flex justify-center xl:hidden relative">
             {/* <ProjectBtn /> */}
